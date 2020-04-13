e6f250da61753931025a419dec5e6c1edd4dc373
hydrogen-studio <61718408+hydrogen-studio@users.noreply.github.com>
2020-04-12 22:09:23 -0700

    Update credentials.js

--- config/credentials.js
+++ config/credentials.js
@@ -1,6 +1,6 @@
 module.exports = {
-  GITHUB_CLIENT_ID: '',
-  GITHUB_CLIENT_SECRET: '',
-  GITHUB_CALLBACK_URL: '',
-  DBURL: '',
+  GITHUB_CLIENT_ID: 'f7cbb54925adc02effba',
+  GITHUB_CLIENT_SECRET: 'a53b2452aba02bb74bf74a492e4adbb8a9b3dc87',
+  GITHUB_CALLBACK_URL: 'https://hydrogenbot.xyz/dashboard',
+  DBURL: 'mongodb+srv://Hydrogen:20060203Z@cluster0-2yvre.mongodb.net/test?retryWrites=true&w=majority',
 };
